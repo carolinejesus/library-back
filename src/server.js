8ee0da02b17c2a3a838b6f2dd28de987682e3ad7
Carol <caroline.fjscc@gmail.com>
2025-12-04 08:51:43 -0300

    outras alterações

--- src/server.js
+++ src/server.js
@@ -20,6 +20,7 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    ssl: { rejecUnauthorized: false }
 });
 
 const app = express();
